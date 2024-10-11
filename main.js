6e07676761406642dcf74e38ae8cf89740268dab
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-10-11 12:00:34 -0300

    i

---
+++ main.js
@@ -0,0 +1,17 @@
+function criaCartaogoria, (categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="conteudocartao">
+    <h3>categoria</h3>
+    <div class="perguntacartao">
+        <p>pergunta</p>
+    </div>
+    <div class="respostacartao">
+        <p>resposta</p>
+    </div>
+ </div>
+    `
+    container.appendChild(cartao);
+}  
---
+++ pergunta.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'Matemática',
+    'Como se calcula os termos de uma P.A?',
+    'Utiliza-se a fórmula sn= (a1+an)-n)/2')
+    
+    criaCartao(
+    'História',
+    'Oque é a Revolução Industrial?',
+    'Processos de mudanças tecnológicas e sociais iniciado no século XVIII, na Inglaterra')
+    
+    criaCartao(
+    'Matemática',
+    'Oque é uma função Quadrática?',
+    'Função polinomial de segundo grau expressa por\(f(x) = ax^2 + vc + c\)')
+    
+    criaCartao(
+    'Química',
+    'Quais são os tipos de ligações químicas?',
+    'As ligações químicas podem ser iônicas, covalentes e metálicas')
+    
+    criaCartao(
+    'História',
+    'Oque foi o Renascimento?',
+    'Movimento cultural Europeu (séculos XVI-XVII) valorizando o humanismo e a cultura grego-romana')
+    
+    criaCartao(
+    'Química',
+    'Como se caracteriza uma reação Endotérmica?',
+    'Reação que absorve energia, geralmente na forma de calor')
+    
+    criaCartao(
+    'Física',
+    'Oque são grandezas vetoriais?',
+    'Possuem magnitude, direção e sentido (ex.: força, velocidade.)')
+    
+    criaCartao(
+    'Matemática',
+    'Qual é a fórmula da área de um círculo?',
+    'É dada pela fórmula \(A = \pi r^2\) onde \( r \) é o raio')
+    
+    criaCartao(
+    'História',
+    'Oque foi o Iluminismo?',
+    'Movimento Intelectual do século XVIII, defendendo o uso da razão')
+    
+    criaCartao(
+    'Biologia',
+    'Oque é Fotossíntese?',
+    'Processo de conversão de luz, água e CO2 em glicose e oxigênio')
+    
+    criaCartao(
+    'Sociologia',
+    'Oque são direitos fundamentais?',
+    'Direitos básicos como vida, liberdade e igualdade')
+    
+    criaCartao(
+    'Física',
+    'Qual é o princípio da Inércia?',
+    'Um corpo permanece em repouso ou movimento constante, a menos que uma força externa atue')
+    
+    criaCartao(
+    'Matemática',
+    'Oque é uma Progressão aritmética (PÁ)?',
+    'Sequência numérica com diferença constante entre termos consecutivos')
+    
+    criaCartao(
+    'História',
+    'Oque foi a Guerra Fria?',
+    'Período de tensão política entre EUA e URSS após a Segunda Guerra Mundial')
+    
+    criaCartao(
+    'Química',
+    'Oque é um sistema tampão?',
+    'Solução que resiste a mudanças de pH'),
+    
+    criaCartao(
+    'Língua Portuguesa',
+    'Oque são metáforas?',
+    'Figura de linguagem que compara implicitamente dois elementos')
+    
+    criaCartao(
+    'Química',
+    'Oque é Lei da Conservação de Energia?',
+    'Energia não se cria nem se destrói, apenas se transforma')
+    
+    criaCartao(
+    'Biologia',
+    'Quais são as bases nitrogenadas do DNA?',
+    'Adenina, timina, citosina e guanina')
+    
+    criaCartao(
+    'Física',
+    'Oque é um movimento uniformemente variado (MUV)?',
+    'Movimento com aceleração constante e velocidade variando uniformemente')
+    
+    criaCartao(
+    'História',
+    'Oque foi a Revolução Francesa?',
+    'Movimento que derrubou a monarquia absolutista na França em 1789')
