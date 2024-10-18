0f3a2eee700d5479e9a4781cfe503158603d6d08
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-10-18 12:07:43 -0300

    u

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ function criaCartao(categoria, pergunta, resposta){
     let respostaEstaVisivel = false;
     function viraCartao(){
         respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active', respotaEstaVisivel);
+        cartao.classList.toggle('active', respostaEstaVisivel);
 
     }
     cartao.addEventListener('click', viraCartao);
 
-    
+
     container.appendChild(cartao);
 }  
