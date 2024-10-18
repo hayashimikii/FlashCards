803861d53d3cfec131468bd3a5a8b28244aca229
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-10-18 11:36:42 -0300

    t

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ function criaCartao(categoria, pergunta, resposta){
     cartao.innerHTML = `
     <div class="conteudo-cartao">
     <h3>${categoria}</h3>
-    <div class="perguntacartao">
+    <div class="pergunta-cartao">
         <p>${pergunta}</p>
     </div>
-    <div class="respostacartao">
+    <div class="resposta-cartao">
         <p>${resposta}</p>
     </div>
  </div>
