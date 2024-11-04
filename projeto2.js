50d9744b075c6f1001e3a389bac7ca0001d4a16c
hayashimikii <142352561+hayashimikii@users.noreply.github.com>
2024-11-04 07:54:01 -0300

    i

--- projeto2.js
+++ projeto2.js
@@ -18,7 +18,7 @@ function criaCartao(categoria, pergunta, resposta){
     container.appendChild(cartao);
 }  
 function mostraResposta (card){
-    const resposta = document.querySelector('.resposta-cartao')
+    const resposta = card.querySelector('.resposta-cartao')
 
     if(resposta.style,display === 'none'){
        resposta.style.display = 'block';
